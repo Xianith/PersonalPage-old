a8ba2d3491b73cffe8de8ebee6de91c53ec051c3
Ben Paulson <xianith@gmail.com>
2013-06-12 18:09:21 -0400

    Uploaded original content

---
+++ Insomnia & Misc/config.php
@@ -0,0 +1,16 @@
+<?php
+// phpBB 3.0.x auto-generated configuration file
+// Do not change anything in this file!
+$dbms = 'mysqli';
+$dbhost = 'mysql.forums.thelaughingmandayz.com';
+$dbport = '';
+$dbname = 'forums_thelaughingmanday';
+$dbuser = 'forumsthelaughin';
+$dbpasswd = 'PyixwVSq';
+$table_prefix = 'phpbb_';
+$acm_type = 'file';
+$load_extensions = '';
+
+@define('PHPBB_INSTALLED', true);
+// @define('DEBUG', true);
+// @define('DEBUG_EXTRA', true);
